922a196611aeb3fe6a76c4a1dcfb5163c4ba60fd
farizrivaldo98 <123930917+farizrivaldo98@users.noreply.github.com>
2023-04-03 20:00:57 +0700

    coba validation

--- src/backend/index.js
+++ src/backend/index.js
@@ -4,9 +4,23 @@ const cors = require("cors");
 const port = 8001;
 const app = express();
 const { databaseRouter } = require("./routers");
+const { body, validationResult } = require("express-validator");
 
 app.use(cors());
 app.use(express.json());
+
+app.post(
+  "/validation",
+  body("email").isEmail(),
+  body("password").isLength({ min: 5 }),
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ error: errors.array() });
+    }
+    return res.status(200).send(req.body);
+  }
+);
 
 app.use("/part", databaseRouter);
 
