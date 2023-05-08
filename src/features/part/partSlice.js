3f2a155ad27b19d1a790969ce885158bc8e2926f
farizrivaldo98 <123930917+farizrivaldo98@users.noreply.github.com>
2023-05-08 12:02:27 +0700

    `graph

---
+++ src/features/part/partSlice.js
@@ -0,0 +1,95 @@
+import { createSlice } from "@reduxjs/toolkit";
+import Axios from "axios";
+import { Await } from "react-router-dom";
+
+export const partSlice = createSlice({
+  name: "part",
+  initialState: {
+    partValue: [],
+    partId: {
+      Mesin: "",
+      Line: "",
+      Pekerjaan: "",
+      Detail: "",
+      Tanggal: "",
+      Quantity: 0,
+      Unit: "",
+      Pic: "",
+      Tawal: "",
+      Tahir: "",
+      Total: 0,
+    },
+  },
+  reducers: {
+    setPartList: (state, action) => {
+      state.partValue = action.payload;
+    },
+    addPartList: (state, action) => {
+      state.partValue.push(action.payload);
+    },
+    deletePartList: (state, action) => {
+      state.partValue = action.payload;
+    },
+    editePartList: (state, action) => {
+      state.partValue = action.payload;
+    },
+    partListId: (state, action) => {
+      state.partId = action.payload;
+    },
+  },
+});
+
+export const {
+  setPartList,
+  addPartList,
+  deletePartList,
+  editePartList,
+  partListId,
+} = partSlice.actions;
+export default partSlice.reducer;
+
+export function addPartListData(data) {
+  return async (dispatch) => {
+    let response = await Axios.post("http://10.126.15.135:8001/part/add", data);
+    dispatch(fetchPart());
+  };
+}
+
+export function fetchPart() {
+  return async (dispatch) => {
+    let response = await Axios.get("http://10.126.15.135:8001/part/get");
+    dispatch(setPartList(response.data));
+  };
+}
+
+export function deletePartListData(data) {
+  return async (dispatch) => {
+    let response = await Axios.delete(
+      `http://10.126.15.135:8001/part/delet/${data}`
+    );
+    dispatch(fetchPart());
+  };
+}
+
+export function editePartListData(data, id) {
+  return async (dispatch) => {
+    let response = await Axios.patch(
+      `http://10.126.15.135:8001/part/edit/${id}`,
+      data
+    );
+    dispatch(fetchPart());
+  };
+}
+
+export function getDataById(dataId) {
+  return async (dispatch) => {
+    let response = await Axios.get("http://10.126.15.135:8001/part/get", {
+      params: {
+        id: dataId,
+      },
+    });
+    const found = response.data.find((element) => element.id == dataId);
+
+    dispatch(partListId(found));
+  };
+}
---
+++ src/pages/PareetoLine.jsx
@@ -0,0 +1,65 @@
+import React, { useEffect, useState, Component } from "react";
+import { useSelector } from "react-redux";
+import ParetoChart from "pareto-chart";
+import axios from "axios";
+
+function ParetoLine() {
+  const [data1, setData] = useState();
+  const [paretoData, paretoSetData] = useState();
+  const [line1, setline1] = useState();
+  const [line2, setline2] = useState();
+  const [line3, setline3] = useState();
+  const [line4, setline4] = useState();
+
+  const fetchData = async () => {
+    // class sakaFarmaPlant {
+    //   constructor(Line1, Line2, Line3, Line4) {
+    //     this.Line1 = Line1;
+    //     this.Line2 = Line2;
+    //     this.Line3 = Line3;
+    //     this.Line4 = Line4;
+    //   }
+    // }
+    // const dataParetoSaka = new sakaFarmaPlant(
+    //   data1[0].total,
+    //   data1[1].total,
+    //   data1[2].total,
+    //   data1[3].total
+    // );
+    // console.log(dataParetoSaka);
+    // paretoSetData(dataParetoSaka);
+
+    let response = await axios.get("http://10.126.15.135:8001/part/pareto");
+
+    //setData(response.data);
+
+    setline1(Number(response.data[0].y));
+    setline2(Number(response.data[1].y));
+    setline3(Number(response.data[2].y));
+    setline4(Number(response.data[3].y));
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <div class="p-20">
+      <ParetoChart
+        width={3}
+        height={1}
+        lineLabel="Cumulative percentage"
+        data={{
+          "Saka Farma Plant": {
+            "Line 1": line1,
+            "Line 2": line2,
+            "Line 3": line3,
+            "Line 4": 0,
+          },
+        }}
+      />
+    </div>
+  );
+}
+
+export default ParetoLine;
