1d1b126dcae3d284f7728c926cac84b2f6a4b697
farizrivaldo98 <123930917+farizrivaldo98@users.noreply.github.com>
2023-06-02 10:57:24 +0700

    Update Maintenance.jsx

--- src/pages/Maintenance.jsx
+++ src/pages/Maintenance.jsx
@@ -12,16 +12,23 @@ import {
 } from "@chakra-ui/react";
 import MachineBreakdown from "./MachineBreakdown";
 import HandoverMaintenance from "./HandoverMaintenance";
+import { useSelector } from "react-redux";
 function Maintenance() {
+  const userGlobal = useSelector((state) => state.user.user);
   return (
     <Card>
       <CardBody>
         <Tabs isFitted size={"lg"} variant="enclosed" class=" p-3  ">
           <TabList>
-            <Tab className="font-bold">Maintenance Report</Tab>
-            <Tab className="font-bold">Maintenance Breakdown Pareto</Tab>
-
-            <Tab className="font-bold">Data Report</Tab>
+            {userGlobal.level == 1 || 2 ? (
+              <Tab className="font-bold">Maintenance Breakdown Pareto</Tab>
+            ) : (
+              <>
+                <Tab className="font-bold">Maintenance Report</Tab>
+                <Tab className="font-bold">Maintenance Breakdown Pareto</Tab>
+                <Tab className="font-bold">Data Report</Tab>
+              </>
+            )}
           </TabList>
 
           <TabPanels>
