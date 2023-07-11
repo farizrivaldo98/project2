ce3256fc58307e6e5a1fca94b26f6b71646f404d
farizrivaldo98 <123930917+farizrivaldo98@users.noreply.github.com>
2023-07-11 13:37:47 +0700

    v 11.07.13.37

--- src/pages/HVAC.jsx
+++ src/pages/HVAC.jsx
@@ -1,7 +1,41 @@
 import React from "react";
+import {
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel,
+  Card,
+  CardBody,
+} from "@chakra-ui/react";
 
 function HVAC() {
-  return <div>HVAC</div>;
+  return (
+    <div>
+      <Card>
+        <CardBody>
+          <Tabs isFitted size={"lg"} variant="enclosed" class=" p-3  ">
+            <TabList>
+              <Tab>CHILLER</Tab>
+              <Tab>AIR HANDLING</Tab>
+              <Tab>HEATING</Tab>
+            </TabList>
+            <TabPanels>
+              <TabPanel>
+                <p>CHILLER</p>
+              </TabPanel>
+              <TabPanel>
+                <p>AIR HANDLING</p>
+              </TabPanel>
+              <TabPanel>
+                <p>HEATING</p>
+              </TabPanel>
+            </TabPanels>
+          </Tabs>
+        </CardBody>
+      </Card>
+    </div>
+  );
 }
 
 export default HVAC;
