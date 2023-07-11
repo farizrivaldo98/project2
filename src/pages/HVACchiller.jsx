a192bc57df1a6aef77c8df6092da57c9e7d7027a
farizrivaldo98 <123930917+farizrivaldo98@users.noreply.github.com>
2023-07-11 20:35:45 +0700

    11.07.20.15

--- src/pages/HVACchiller.jsx
+++ src/pages/HVACchiller.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 function HVACchiller() {
-  return <div>HVACchiller</div>;
+  return (
+    <div>
+      <div>
+        <br />
+        <Button className="ml-4" colorScheme="gray">
+          Show All Data
+        </Button>
+        <Button className="ml-4" colorScheme="gray">
+          Hiden All Data
+        </Button>
+      </div>
+    </div>
+  );
 }
 
 export default HVACchiller;
