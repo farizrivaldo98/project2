cf0b2d617fd33b9bac3d80afed6f0c6f5977f7ff
farizrivaldo98 <123930917+farizrivaldo98@users.noreply.github.com>
2023-08-16 10:21:16 +0700

    rubah gatau mana

--- src/pages/building.jsx
+++ src/pages/building.jsx
@@ -1,7 +1,37 @@
 import React from "react";
+import {
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel,
+  Card,
+  CardBody,
+} from "@chakra-ui/react";
 
 function building() {
-  return <div>building</div>;
+  return (
+    <div>
+      <Card>
+        <CardBody>
+          <Tabs isFitted size={"lg"} variant="enclosed" class=" p-3  ">
+            <TabList>
+              <Tab>Enviroment Management System</Tab>
+              <Tab>Building Management System</Tab>
+            </TabList>
+            <TabPanels>
+              <TabPanel>
+                <h1>EMS</h1>
+              </TabPanel>
+              <TabPanel>
+                <h1>BAS</h1>
+              </TabPanel>
+            </TabPanels>
+          </Tabs>
+        </CardBody>
+      </Card>
+    </div>
+  );
 }
 
 export default building;
