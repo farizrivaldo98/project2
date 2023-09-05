2e9603b82d6a08c4766274a02e41a18497a71007
farizrivaldo98 <123930917+farizrivaldo98@users.noreply.github.com>
2023-09-05 08:45:48 +0700

    sw6

--- src/pages/Stopwatch.jsx
+++ src/pages/Stopwatch.jsx
@@ -1,57 +1,46 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useRef } from "react";
 
 const Stopwatch = () => {
-  const [minutes, setMinutes] = useState(0);
-  const [seconds, setSeconds] = useState(0);
-  const [milliseconds, setMilliseconds] = useState(0);
+  const [time, setTime] = useState(0);
   const [isRunning, setIsRunning] = useState(false);
   const intervalRef = useRef(null);
+  const startTimeRef = useRef(0);
 
-  useEffect(() => {
-    if (isRunning) {
-      intervalRef.current = setInterval(() => {
-        setMilliseconds((prevMilliseconds) => {
-          if (prevMilliseconds === 99) {
-            setSeconds((prevSeconds) => {
-              if (prevSeconds === 59) {
-                setMinutes((prevMinutes) => prevMinutes + 1);
-                return 0;
-              }
-              return prevSeconds + 1;
-            });
-            return 0;
-          }
-          return prevMilliseconds + 1;
-        });
-      }, 1);
-    } else {
-      clearInterval(intervalRef.current);
-    }
+  const formatTime = (milliseconds) => {
+    const totalSeconds = Math.floor(milliseconds / 1000);
+    const minutes = Math.floor(totalSeconds / 60);
+    const seconds = totalSeconds % 60;
+    const millis = milliseconds % 1000;
+    return `${minutes.toString().padStart(2, "0")}.${seconds
+      .toString()
+      .padStart(2, "0")}.${millis.toString().padStart(3, "0")}`;
+  };
 
-    return () => {
-      clearInterval(intervalRef.current);
-    };
-  }, [isRunning]);
+  const updateElapsedTime = () => {
+    const currentTime = Date.now();
+    const elapsedTime = isRunning
+      ? time + (currentTime - startTimeRef.current)
+      : time;
+    setTime(elapsedTime);
+    startTimeRef.current = currentTime;
+  };
 
   const handleStartPause = () => {
+    if (!isRunning) {
+      startTimeRef.current = Date.now();
+    }
     setIsRunning((prevState) => !prevState);
   };
 
   const handleReset = () => {
     clearInterval(intervalRef.current);
     setIsRunning(false);
-    setMinutes(0);
-    setSeconds(0);
-    setMilliseconds(0);
+    setTime(0);
   };
 
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-6xl font-bold">
-        {minutes.toString().padStart(2, "0")}.
-        {seconds.toString().padStart(2, "0")}.
-        {milliseconds.toString().padStart(3, "0")}
-      </h1>
+      <h1 className="text-6xl font-bold">{formatTime(time)}</h1>
       <div className="space-x-4">
         <button
           className={`bg-green-500 hover:bg-green-600 text-white px-8 py-4 rounded ${
