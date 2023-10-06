e255a959a755afcf31fa866616994f2baa061379
farizrivaldo98 <123930917+farizrivaldo98@users.noreply.github.com>
2023-10-06 10:06:37 +0700

    navbar update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -183,7 +183,7 @@ export default function Navbar() {
               <div>
                 <p class="text-white mr-4">{currentDateTimeString}</p>
               </div>
-              {userGlobal.id ? (
+              
                 <>
                   <Menu>
                     <MenuButton className=" flex flex-2 items-end justify-end bg-gray-900 text-white hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">
@@ -207,9 +207,7 @@ export default function Navbar() {
                     </MenuList>
                   </Menu>
                 </>
-              ) : (
-                <div></div>
-              )}
+              
 
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0"></div>
             </div>
