440aa5fb83061a65804c8e36e60610f9197fe925
stanlyfisher <stanlygs617@gmail.com>
2023-12-05 09:21:29 +0700

    update package water 1

--- src/pages/WaterManagement.jsx
+++ src/pages/WaterManagement.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, Component, useState } from "react";
 import CanvasJSReact from "../canvasjs.react";
 import { Button, ButtonGroup, Stack, Input, Select } from "@chakra-ui/react";
 import axios from "axios";
-import { Chart } from "react-google-charts";
+
 
 var CanvasJS = CanvasJSReact.CanvasJS;
 var CanvasJSChart = CanvasJSReact.CanvasJSChart;
