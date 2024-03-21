63301f2de8f11827bd80c5258e082b72a52ed37a
stanlyfisher <stanlygs617@gmail.com>
2024-03-21 10:16:26 +0700

    update preview export data

--- src/pages/WaterExportMonthly.jsx
+++ src/pages/WaterExportMonthly.jsx
@@ -1,11 +1,25 @@
 import React, { useEffect, Component, useState } from "react";
 import CanvasJSReact from "../canvasjs.react";
-import { Button, ButtonGroup, Stack, Input, Select, Radio, RadioGroup} from "@chakra-ui/react";
+import { Button, 
+    ButtonGroup, 
+    Stack, 
+    Input, 
+    Select, 
+    Radio, 
+    RadioGroup, 
+    Table, 
+    Thead, 
+    Tbody, 
+    Tr, 
+    Th, 
+    Td, 
+    TableContainer 
+} from "@chakra-ui/react";
 import axios from "axios";
 import { ExportToExcel } from "../ExportToExcel";
 
 function WaterExportMonthly() {
-    const [data, setData] = useState([])
+    const [dataExport, setData] = useState([])
     const [startDate, setStartDate] = useState();
     const [finishDate, setFinishDate] = useState();
     const [fileName, setfilename] = useState();
@@ -45,6 +59,35 @@ function WaterExportMonthly() {
         var dataInput = e.target.value;
         setFinishDate(dataInput);
     }; 
+
+    const previewTable = () => {
+        return dataExport.map((data) =>{
+            return (
+                <Tr>
+                    <Td>{data.Bulan}</Td>
+                    <Td>{data.Domestik}</Td>
+                    <Td>{data.Chiller}</Td>
+                    <Td>{data.Softwater}</Td>
+                    <Td>{data.Boiler}</Td>
+                    <Td>{data.Inlet_Pretreatment}</Td>
+                    <Td>{data.Outlet_Pretreatment}</Td>
+                    <Td>{data.Reject_Osmotron}</Td>
+                    <Td>{data.Taman}</Td>
+                    <Td>{data.Inlet_WWTP_Kimia}</Td>
+                    <Td>{data.Inlet_WWTP_Biologi}</Td>
+                    <Td>{data.Outlet_WWTP}</Td>
+                    <Td>{data.CIP}</Td>
+                    <Td>{data.Hotwater}</Td>
+                    <Td>{data.Lab}</Td>
+                    <Td>{data.Atas_Toilet_Lt2}</Td>
+                    <Td>{data.Atas_Lab_QC}</Td>
+                    <Td>{data.Workshop}</Td>
+                    <Td>{data.Air_Mancur}</Td>
+                </Tr>
+            );
+        });
+    };
+
     return (
         <div>
             <div align="center"><h1 style={{ fontSize: "2rem"}}><b>Export Monthly Water Data </b></h1></div>
@@ -88,9 +131,38 @@ function WaterExportMonthly() {
                 align="center"
             >
                 <div>
-                    <ExportToExcel apiData={data} fileName={fileName} />
+                    <ExportToExcel apiData={dataExport} fileName={fileName} />
                 </div>
             </Stack>
+            <div align="center"><h1 style={{ fontSize: "2rem"}}><b>Preview {fileName} :</b></h1></div>
+            <TableContainer>
+          <Table variant="simple">
+            <Thead>
+              <Tr backgroundColor="aliceblue">
+                <Th>Date Time</Th>
+                <Th>Domestik</Th>
+                <Th>Chiller</Th>
+                <Th>Softwater</Th>
+                <Th>Boiler</Th>
+                <Th>Inlet Pretreatment</Th>
+                <Th>Outlet Pretreatment</Th>
+                <Th>Reject Osmotron</Th>
+                <Th>Taman</Th>
+                <Th>Inlet WWTP Kimia</Th>
+                <Th>Inlet WWTP Biologi</Th>
+                <Th>Outlet WWTP</Th>
+                <Th>CIP</Th>
+                <Th>Hotwater</Th>
+                <Th>Lab</Th>
+                <Th>Atas Toilet Lt2</Th>
+                <Th>Atas Lab QC</Th>
+                <Th>Workshop</Th>
+                <Th>Air Mancur</Th>
+              </Tr>
+            </Thead>
+            <Tbody>{previewTable()}</Tbody>
+          </Table>
+        </TableContainer>
         </div>
     );
 }
